4d5f24b2c0573e70c27ac054e52da643a17209d3
Stephen Chen <g1222888@gmail.com>
2017-09-28 20:22:18 +0800

    update script

--- update-lambda.sh
+++ update-lambda.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-zip -r Vbot-notify *
+echo -n "zip project ..."
+zip -rq Vbot-notify *
+
+echo -ne "\r\033[K"
+echo "update lambda ..."
 aws lambda update-function-code --function-name Vbot-notify --zip-file "fileb://Vbot-notify.zip"
+
+
 rm -rf ./Vbot-notify.zip
