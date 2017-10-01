bb2b6d7928f59bd87f98661df81f43103808db14
Stephen Chen <g1222888@gmail.com>
2017-10-01 14:35:37 +0800

    github new version util

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const db = require('./lib/db')
+const site = require('./lib/site-utils')
 
 exports.handler = function(event, context, callback) {
     const checkTime = new Date()
@@ -16,8 +17,17 @@ exports.handler = function(event, context, callback) {
             query DB project subscriptor
             use Chat platform util to notify subscriber
         */
+        Promise.all(projectList.map((project) => {
+            const projectPlatform = db.getProjectFromConstant(project.platform)
+            return site.siteUtil(projectPlatform).getNewVersionInfoPromise(checkTime, project)
+            .then((info) => {
 
-        callback(null)
+            })
+        }))
+        .then((result) => {
+            callback(null)
+        })
+        .catch((err) => callback(err))
     })
 
 }
