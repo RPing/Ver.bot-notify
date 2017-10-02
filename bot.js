90e9c89f65f17368f27550ec6be55e3ed66af2a9
Stephen Chen <g1222888@gmail.com>
2017-10-02 18:40:50 +0800

    database query project subscriber

--- bot.js
+++ bot.js
@@ -14,13 +14,19 @@ exports.handler = function(event, context, callback) {
         /*
             loop:
             use Project platform util to get new version info
-            query DB project subscriptor
+            query DB project subscriber
             use Chat platform util to notify subscriber
         */
         Promise.all(projectList.map((project) => {
-            const projectPlatform = db.getProjectFromConstant(project.platform)
+            const projectPlatform = db.projectPlatform(project.platform)
+
             return site.siteUtil(projectPlatform).getNewVersionInfoPromise(checkTime, project)
             .then((info) => {
+                if (info.length !== 0) {
+                    return db.getAllSubscriberPromise(project.project_name)
+                }
+            })
+            .then((subscribers) => {
 
             })
         }))
